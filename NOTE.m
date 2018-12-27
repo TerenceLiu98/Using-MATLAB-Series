dedf305ec47d19579c21956a1035cd9f3a314e82
Terence Lau <terenceliu1012@gmail.com>
2018-12-28 01:55:46 +0800

    update

---
+++ NOTE.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%BASIC ARITHMETIC%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+5 + 5 % 5 plus 5
+3 ^ 2 % square of 3 
+sin(pi/2) % sine of pi/2(90 degree)
+x = 2  % without semi-colon
+x = 2; % semi-colon 
+namelengthmax
+x = 7 * 8; 
+y = x * 7.89
+x = 7 * 8; 
+x * 7.89
+ans = 1 
